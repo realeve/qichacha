65af529ec5ad31440faf3f029ce3a687caaac2e0
realeve <realeve@qq.com>
2017-09-20 23:50:27 +0800

    细节修复

--- controller/task.js
+++ controller/task.js
@@ -6,9 +6,9 @@ let score = require('./spider/companyScore');
 async function init() {
     console.log('系统初始化：数据库表单初始化，载入默认数据。此处哪项任务未完成则请自行取消注释信息.');
     
-    // qichacha.init();
+    qichacha.init();
     
-    score.init();
+    // score.init();
 }
 
 module.exports = {
